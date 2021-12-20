07b1911bb1136520d470b7c3dffa512f25f3f99a
Andres <andres.nazarian@hotmail.com>
2021-12-20 11:25:05 -0300

    Updates

---
+++ precache-manifest.9a56fe0699c4fe4597c8f039e6f2f37c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d5e3c5dedc4c64bdaa1f5cce6f2d12e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f6766ca07a69e5fc2ca7",
+    "url": "/static/css/main.d1a74864.chunk.css"
+  },
+  {
+    "revision": "1f0451578e7152f6ade0",
+    "url": "/static/js/2.e7012397.chunk.js"
+  },
+  {
+    "revision": "f6766ca07a69e5fc2ca7",
+    "url": "/static/js/main.e7db377a.chunk.js"
+  },
+  {
+    "revision": "0a31ddf7382e0363a369",
+    "url": "/static/js/runtime-main.b240f91f.js"
+  }
+]);
